2e784f7d5442c806f533a172c100619604ccf4c5
AmmarlOsama <ammarosama011@gmail.com>
2021-12-15 07:59:05 +0200

    creating the template folder in the cart app and creating the cart_detail.html

--- cart/cart.py
+++ cart/cart.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 from django.conf import settings
-from shop.models import product
+from shop.models import Product
 
 
 
--- cart/views.py
+++ cart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cart import Cart
-from .forms import Cart CartAddProductForm
+from .forms import CartAddProductForm
 
 
 # Create your views here.
