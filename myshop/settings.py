e81c0ccbc75a848d0ecb2870ddc2b55b8ce7939a
AmmarlOsama <ammarosama011@gmail.com>
2021-12-16 01:16:36 +0200

    adding whitenoise

--- myshop/settings.py
+++ myshop/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     
 ]
 
@@ -129,6 +131,8 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #the key where we store the cart of user session
 CART_SESSION_ID = 'cart'
