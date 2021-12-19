64e5f659e31c3aac35a087a208d772e7057ab79d
AmmarlOsama <ammarosama011@gmail.com>
2021-12-19 15:31:15 +0200

    creating a tasks.py and adding a task to views . py in ther Orders app

--- myshop/celery.py
+++ myshop/celery.py
@@ -5,5 +5,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE','myshop.settings')
 
 app = Celery('myshop')
 
-app.config_from_object('django.config:settings',namespace='CELERY')
+app.config_from_object('django.conf:settings',namespace='CELERY')
 app.autodiscover_tasks()
--- myshop/settings.py
+++ myshop/settings.py
@@ -138,3 +138,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CART_SESSION_ID = 'cart'
 
 django_heroku.settings(locals())
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
