57aa8b1aa65e5994224e069614f17d4672522e94
AmmarlOsama <ammarosama011@gmail.com>
2022-01-03 18:04:10 +0200

    adding WeasyPrint to requirements.in and installing it

--- orders/admin.py
+++ orders/admin.py
@@ -8,6 +8,11 @@ import csv
 import datetime
 from django.http import HttpResponse
 
+#adding a custome view to display order details
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -35,14 +40,17 @@ def export_to_csv(modeladmin,request,queryset) :
         writer.writerow(data_row)
     return response
 export_to_csv.short_description = 'Export to CSV'
-    
 
-    
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail',args=[obj.id])
+    return mark_safe(f'<a href="{url}">View Order</a>')
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id','first_name','last_name','email',
                     'address','postal_code','city','paid',
-                    'created','updated']
+                    'created','updated',order_detail]
     list_filter = ['paid','created','updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
+
--- orders/models.py
+++ orders/models.py
@@ -5,6 +5,8 @@ from django.db import models
 from django.db import models
 from shop.models import Product
 
+
+
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
